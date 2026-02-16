7cb6b7b6edb1b96732c33198a379c964d8cdd2ab
Anubrata <anubratabhattacharyya81@gmail.com>
2026-02-16 15:38:48 +0530

    Configured PostgreSQL database

--- studybud/settings.py
+++ studybud/settings.py
@@ -59,8 +59,13 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 
 # DATABASE
 DATABASES = {
-    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 # PASSWORD VALIDATION
 AUTH_PASSWORD_VALIDATORS = [
