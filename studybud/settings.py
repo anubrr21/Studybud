72a3b58abe1e35041a8cbc35f23a5550c0b25e0c
Anubrata <anubratabhattacharyya81@gmail.com>
2026-02-17 00:43:55 +0530

    Fix database config for local as well as production

--- studybud/settings.py
+++ studybud/settings.py
@@ -55,14 +55,22 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'studybud.wsgi.application'
 
-# DATABASE
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 
 # PASSWORD VALIDATION
