bf6c0191f6ee62b17a712a3a12a98323849e3d43
Anubrata <anubratabhattacharyya81@gmail.com>
2026-02-20 00:30:12 +0530

    Like feature has been fixed

--- base/urls.py
+++ base/urls.py
@@ -14,6 +14,6 @@ urlpatterns=[
     path('update-user/',views.updateUser,name="update-user"),
     path('topics/',views.topicsPage,name="topics"),
     path('activity/',views.activityPage,name="activity"),
-    path('like-room/<str:pk>/', views.toggle_like, name="like-room"),
+    path('like-room/<str:pk>/', views.toggle_like, name="toggle-like"),
     path('notifications/', views.notifications, name='notifications'),
 ]
