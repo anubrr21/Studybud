6ad5652f1927790767ffbfbd4fbb9dd48140a7ea
Anubrata <anubratabhattacharyya81@gmail.com>
2026-02-21 00:07:09 +0530

    Fixed notif

--- studybud/settings.py
+++ studybud/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 
 # STATIC FILES
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / "base/static",]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
