6876b71243b60acf70a8ca47db156eb22c732636
Anubrata <anubratabhattacharyya81@gmail.com>
2026-02-21 23:56:44 +0530

    the fix for static files updated

--- studybud/settings.py
+++ studybud/settings.py
@@ -95,7 +95,7 @@ USE_TZ = True
 
 # STATIC FILES
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "base/static",]
+STATICFILES_DIRS = [BASE_DIR / "static",]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
