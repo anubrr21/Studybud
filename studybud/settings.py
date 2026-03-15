cbcbf2345ee5f2bfe8f245c01d2b267fbccd7996
Anubrata <anubratabhattacharyya81@gmail.com>
2026-03-16 03:35:28 +0530

    Fixed migrations issues

--- studybud/settings.py
+++ studybud/settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base.apps.BaseConfig',
+    'base',
     'rest_framework',
     'corsheaders',
     'channels',
